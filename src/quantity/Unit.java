b390738f1320c005a945c3cb55743526d9452658
Fred George <fredgeorge@acm.org>
2019-02-11 16:03:22 +0100

    Fred: Quantity created with Unit convenience constructors; Unit still not exploited

--- src/quantity/Unit.java
+++ src/quantity/Unit.java
@@ -3,6 +3,24 @@ package quantity;
 // Understands a particular metric
 public class Unit {
     public static final Unit TEASPOON = new Unit();
-    public static final Unit TABLESPOON = new Unit();
+    public static final Unit TABLESPOON = new Unit(3, TEASPOON);
+    public static final Unit OUNCE = new Unit(2, TABLESPOON);
+    public static final Unit CUP = new Unit(8, OUNCE);
+    public static final Unit PINT = new Unit(2, CUP);
+    public static final Unit QUART = new Unit(2, PINT);
+    public static final Unit GALLON = new Unit(2, QUART);
+
+    private final double baseUnitRatio;
+
+    private Unit() { baseUnitRatio = 1.0; }
+
+    private Unit(double relativeRatio, Unit relativeUnit) {
+        baseUnitRatio = relativeRatio * relativeUnit.baseUnitRatio;
+    }
+
+    public Quantity s(double amount) {
+        return new Quantity(amount, this);
+    }
+
 
 }
