9f68b2b7afb84e248dcc523483e2b8597346eadd
Fred George <fredgeorge@acm.org>
2019-02-14 10:00:29 +0100

    Fred: Graph with minimum hopCount using streams

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -31,18 +31,12 @@ public class Node {
     private double hopCount(Node destination, List<Node> visitedNodes) {
         if (this == destination) return 0;
         if (visitedNodes.contains(this)) return UNREACHABLE;
-        return neighborHopCount(destination, visitedNodes);
+        return neighbors.stream()
+                .mapToDouble(n -> n.hopCount(destination, copyWithThis(visitedNodes)) + 1)
+                .min()
+                .orElse(UNREACHABLE);
     }
-
-    private double neighborHopCount(Node destination, List<Node> visitedNodes) {
-        var champion = UNREACHABLE;
-        for (Node n: neighbors) {
-            var challenger = n.hopCount(destination, copyWithThis(visitedNodes)) + 1;
-            if (challenger < champion) champion = challenger;
-        }
-        return champion;
-    }
-
+    
     private List<Node> noVisitedNodes() {
         return new ArrayList<>();
     }
